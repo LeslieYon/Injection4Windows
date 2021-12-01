97719d262887ceef03f86fb8a7ae661db4287f5d
LeslieYon <brightmbsh@gmail.com>
2021-12-02 05:16:21 +0800

    Add MemWrite Function

--- injection.cpp
+++ injection.cpp
@@ -7,21 +7,27 @@
  */
 int JustLoadlibrary(PROCESS_INFORMATION& pi)
 {
-
+    //将要加载的模块名称字符串写入目标进程
     TCHAR szDllname[] = TEXT("libtestDll.dll");
     void* remoteAddr = VirtualAllocEx(pi.hProcess, nullptr,sizeof(szDllname),MEM_RESERVE | MEM_COMMIT,
                                       PAGE_EXECUTE_READWRITE|PAGE_NOCACHE);
     if (!remoteAddr)
     {
         printf("VirtualAlloc failed (%d).\n", GetLastError());
+        return false;
     }
     WriteProcessMemory(pi.hProcess,remoteAddr,szDllname,sizeof(szDllname), nullptr);
+    //创建新线程，直接调用LoadLibrary加载需要注入的模块
     HANDLE hRemoteThreadInject = CreateRemoteThread(pi.hProcess, NULL, 0, (LPTHREAD_START_ROUTINE)LoadLibrary, remoteAddr, 0, NULL);
+    //等待远程模块加载过程结束
     WaitForSingleObject(hRemoteThreadInject, INFINITE);
+    //释放模块名称字符串在目标进程所占的空间
     if (!VirtualFreeEx(pi.hProcess,remoteAddr,sizeof(szDllname),MEM_DECOMMIT))
     {
         printf("VirtualFree failed (%d).\n", GetLastError());
+        return false;
     }
+    //获取远程模块加载加载是否成功的结果
     DWORD InjectDllBaseAddr = 0;
     GetExitCodeThread(hRemoteThreadInject, &InjectDllBaseAddr);
     if (InjectDllBaseAddr)
@@ -31,9 +37,10 @@ int JustLoadlibrary(PROCESS_INFORMATION& pi)
     else
     {
         printf("Failed to load remote Dll\n");
-        return 0;
+        return false;
     }
     Sleep(5000);
+    //利用FreeLibrary卸载远程加载的模块
     HANDLE hRemoteThreadFree = CreateRemoteThread(pi.hProcess,NULL,0,(LPTHREAD_START_ROUTINE)FreeLibrary,(LPVOID)InjectDllBaseAddr,0,NULL);
     WaitForSingleObject(hRemoteThreadFree, INFINITE);
     DWORD IsFreeSuccess = 0;
@@ -42,25 +49,73 @@ int JustLoadlibrary(PROCESS_INFORMATION& pi)
     {
         printf("Remote dll has unloaded!\n");
     }
-    return 0;
+    return true;
 }
 
 /*
  * 通过直接写入进程内存的方式，注入自己的模块。
  */
+LPCSTR lpModuleName;
 int MemWrite(PROCESS_INFORMATION& pi)
 {
-
+    //获取当前进程加载基址
+    HANDLE handle = GetModuleHandleA(lpModuleName);
+    PointersToPEBuffer BufferInfo;
+    GetBufferInfo(handle, BufferInfo, BufferType::ImageBuffer);
+    void* NewImageBuffer = malloc(BufferInfo.PEBufferSize);
+    memcpy(NewImageBuffer,handle,BufferInfo.PEBufferSize);
+    //在目标进程中分配内存空间
+    void* ImageLoadAddr = VirtualAllocEx(pi.hProcess,nullptr, BufferInfo.PEBufferSize, MEM_RESERVE | MEM_COMMIT,PAGE_EXECUTE_READWRITE);
+    //根据分配的内存空间位置，修复重定位
+    if (!RebuildRelocationTable(NewImageBuffer, BufferType::ImageBuffer, ImageLoadAddr)) //重定位PE文件
+    {
+        Error("Can't rebulid Src PE relocation!");
+        return false;
+    }
+    //将PE文件镜像写入目标进程
+    if (!WriteProcessMemory(pi.hProcess, ImageLoadAddr, NewImageBuffer, BufferInfo.PEBufferSize, nullptr)) {
+        Error("Can't write Src PE file to new process!");
+        return false;
+    }
+    //计算注入的PE文件的入口点的位置
+    DWORD EntryAddress = (DWORD)GetProcAddress((HMODULE)handle,"Entry@4")-(DWORD)handle+(DWORD)ImageLoadAddr;
+    //远程新建一个线程，使其从入口点的位置开始执行
+    //为了方便，此处直接将注入模块的加载位置作为参数传递给入口函数
+    HANDLE RemoteThreadHandle = CreateRemoteThread(pi.hProcess,NULL,0,(LPTHREAD_START_ROUTINE)EntryAddress,ImageLoadAddr,0,NULL);
+    //等待远程线程执行结束
+    WaitForSingleObject(RemoteThreadHandle,INFINITE);
+    DWORD szExitCode = 0;
+    GetExitCodeThread(RemoteThreadHandle,&szExitCode);
+    printf("Remote Thread has exited with code 0x%08x\n",szExitCode);
+    //释放目标进程中申请的内存空间
+    if (!VirtualFreeEx(pi.hProcess,ImageLoadAddr,BufferInfo.PEBufferSize,MEM_DECOMMIT))
+    {
+        printf("VirtualFree failed (%d).\n", GetLastError());
+    }
     return 0;
 }
 
-//通过方式2注入进程时，模块开始执行的入口点
-extern "C" __declspec(dllexport) void WINAPI Entry()
+/*
+ * 通过方式2注入进程时，模块开始执行的入口点，
+ * 此函数原型应与ThreadProc函数一致。
+ * 此函数返回值作为线程退出代码传递给父进程
+ */
+extern "C" __declspec(dllexport) DWORD WINAPI Entry(DWORD ImageBase)
 {
-    MessageBox(nullptr,TEXT("Entry Point!"), nullptr,0);
-    return;
+    //注入代码运行第一步：重新修复导入表
+    if (!BuildImportTable((void*)ImageBase,BufferType::ImageBuffer))
+    {
+        MessageBox(nullptr,TEXT("Build ImportTable Failed!"), TEXT("Error"),0);
+        return false;
+    }
+    //注入代码运行第二步：执行真正的代码
+    MessageBox(nullptr,TEXT("Entry Point!"), TEXT("Success"),0);
+    return true;
 }
 
+/*
+ * 创建需要注入的目标进程。
+ */
 bool createProcess(PROCESS_INFORMATION& pi)
 {
     const TCHAR* szApplicationName = TEXT("..\\SrcTest.exe");
@@ -86,7 +141,7 @@ bool createProcess(PROCESS_INFORMATION& pi)
     return true;
 }
 
-int main() {
+int main(int argc, char *argv[]) {
     //创建待注入的进程
     PROCESS_INFORMATION pi;
     createProcess(pi);
@@ -95,6 +150,7 @@ int main() {
     JustLoadlibrary(pi);
 
     //注入方式2
+    lpModuleName = argv[0]; //获取当前模块名
     MemWrite(pi);
 
     return 0;
